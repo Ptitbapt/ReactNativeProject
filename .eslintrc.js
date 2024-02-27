14c1b2bd02671402c556769f4a67cd537924bebe
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2024-02-27 09:18:24 +0100

    Setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
 };
